0c0b80d1ffc101b64ad8f05bf284ceb0ad25e8c2
Michael Halfmoon <michael-halfmoon@utulsa.edu>
2017-03-27 17:52:56 -0500

    Completed and working

--- test.py
+++ test.py
@@ -1,27 +1,13 @@
-def collatz(n):
-  i = 0 
-  while n > 1:
-    if n % 2 == 0:
-      n = n/2
-      i += 1
-    else:
-      n = 3*n+1
-      i += 1
-  return i 
-
 def test(got,expected):
   if got == expected:
     prefix = ' OK '
   else:
     prefix = ' X '
   print '%s got: %s expected: %s' % (prefix, repr(got), repr(expected))
-
 def main():
   print
   print 'collatz'
-  test(collatz(4),2)
-  test(collatz(3),7)
-  test(collatz(32),5)
-
+  test(collatz(4),'2')
+  test(collatz(3),'7')
 if __name__ == '__main__':
-  main()   
+  main()
